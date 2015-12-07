45b4b1d53506183c99d585704ee1ee6035a6d168
Joe Eaves <jinux@alluha.net @soul>
2015-12-07 21:48:08 +0000

    Added package.json and wallet balance.

--- stats.js
+++ stats.js
@@ -3,56 +3,10 @@
 var fs      = require('fs'),
 	path    = require('path'),
 	os      = require('os'),
-	evenode = require('../evenode'),
+	evenode = require('evenode'),
 	cheerio = require('cheerio'),
-	async   = require('async');
-
-// if (process.argv.length < 3) {
-// 	return usage();
-// }
-
-// var argv = process.argv.slice(2);
-
-// var cmd = argv[0];
-
-// if (cmd === 'help') {
-// 	return usage();
-// }
-
-// var apiArgs = argv.slice(1);
-// apiArgs.push(function (err, data) {
-// 	if (err) return console.error(err);
-// 	var x = xml.parseString(data);
-// 	var t = function(err, root) {
-// 		console.log(data);
-// 		console.log(err);
-// 		console.log(root);
-// 	};
-// 	var y = [];
-// 	for (var xx in x.childs) {
-// 		if (x.childs[xx] != '\r\n' && x.childs[xx] != '\r\n  ') {
-// 			y.push(x.childs[xx]);
-// 		}
-// 	}
-// 	x['childs'] = y;
-// 	console.log(x);
-// });
-
-
-// 	var parts = cmd.split(':'),
-// 		namespace = parts[0],
-// 		method = parts[1];
-
-// 	var func = evenode(config)[namespace][method];
-
-// 	func.apply(null, apiArgs);
-// });
-
-let EvePilot = function(char_name, char_id) {
-	this.name    = char_name;
-	this.charid  = char_id;
-}
-
+	async   = require('async'),
+	EventEmitter2 = require('eventemitter2').EventEmitter2;
 
 class Config {
 	constructor(path) {
@@ -61,43 +15,103 @@ class Config {
 	get config() { return this._config; }
 }
 
-class NuxEve {
-	constructor(config, callback) {
-		this._eve   = evenode(config);
-		this._chars = {"loaded": false};
-		this.loadCharacters(() => {
-			callback();
+let EvePilot = function(char_name, char_id) {
+	this.name    = char_name;
+	this.charid  = char_id;
+}
+
+class NuxEve extends EventEmitter2 {
+	constructor(config) {
+		super();
+		this._eve    = evenode(config);
+		this._chars  = new Map();
+		this._loaded = false;
+		this.loadCharacters(() => { 
+			this._loaded = true;
+			this.emit('charsLoaded', this._chars);
+		});
+	}
+
+	_loadData(data, callback) {
+		let $ = cheerio.load(data, {xmlMode: true});
+		let $rows = $('eveapi > result > rowset > row');
+		if($rows.length <= 0)
+			return callback({'err': 'no rows found'});
+
+		$rows.each((index, element) => {
+			callback(null, $(element)[0].attribs)
 		});
 	}
 
 	loadCharacters(callback) {
+		var callback = callback || () => {};
+
 		let queue = async.queue((task, queue_callback) => {
-			this._chars[task.name] = new EvePilot(task.name, task.characterID);
+			this._chars.set(task.name, new EvePilot(task.name, task.characterID));
 			queue_callback();
-		})
+		});
+
 		this._eve.account.characters((err, data) => {
-			console.log(data);
-			let $ = cheerio.load(data, {xmlMode: true});
-			$('eveapi > result > rowset > row').each((index, element) => {
-				let eleData = $(element)[0].attribs;
-				queue.push({"name": eleData.name, "characterID": eleData.characterID});
+			if(err) return callback(err);
+
+			this._loadData(data, (err_two, loadedData) => {
+				if(err) return callback(err);
+
+				queue.push({
+					"name": loadedData.name, 
+					"characterID": loadedData.characterID
+				});
+			})
+		});
+
+		queue.drain = () => {
+			this.loadBalances();
+			this.once('balancesLoaded', callback);
+		};
+
+	}
+
+	loadBalances(callback) {
+		var callback = callback || () => {}; 
+
+		let queue = async.queue((task, queue_callback) => {
+			this._chars.get(task.name)['balance'] = task.balance;
+			queue_callback();
+		});
+
+		this._chars.forEach((value) => {
+			this._eve.character.accountBalance(value.charid, (err, data) => {
+				if(err) return callback(err);
+
+				this._loadData(data, (err_two, loadedData) => {
+					if(err) return callback(err_two);
+
+					queue.push({
+						"name": value.name,
+						"balance": loadedData.balance
+					});
+				})
 			});
 		});
 		queue.drain = () => {
-			this._chars.loaded = true;
+			this.emit('balancesLoaded');
 			callback();
 		};
 	}
 
 	get chars() {
-		if(this._chars.loaded) {
+		if(this._loaded)
 			return this._chars;
-		}
+		return {"error": "Characters not loaded yet!"};
+	}
+
+	getBalance(char_name) {
+		return this._chars;
 	}
 }
 
 let config = new Config(path.join(__dirname, 'config.json'));
-console.log(config.config);
-let ne = new NuxEve(config.config, () => {
+let ne = new NuxEve(config.config);
+ne.once('charsLoaded', () => {
 	console.log(ne.chars);
 });
